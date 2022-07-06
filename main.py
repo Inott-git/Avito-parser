ac9e71ae5017e283a1384f831814df3eae0184bc
Artem <arnst.artyom@gmail.com>
2022-07-06 18:51:37 +0400

    beautiful_commit

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from chromedriver.chrome_check import web_driver
-
+from art import tprint
 
 def main():
+    tprint('parser', font='sub-zero')
+
     # количество обявлений
-    x = int(input())
+    x = int(input('Введите количество объявлений: '))
 
     # запуск веб_драйвера
     web_driver(x)
